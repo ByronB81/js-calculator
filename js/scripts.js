befe6d88e03b2c7e6c0d2e5aaac56f07000ffdd2
Byron Blocker <byronblocker@gmail.com>
2017-05-03 15:06:49 -0700

    create project basics for calculator

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var add = function(x,y) {
+  return x + y;
+};
+
+var sub = function(x,y) {
+  return x - y;
+};
+
+var mult = function(x,y) {
+  return x * y;
+};
+
+var div = function(x,y) {
+  return x / y;
+};
+
+var num1 = parseInt(prompt("Enter a number"));
+var num2 = parseInt(prompt("Enter a second number"));
+alert(add(num1, num2));
